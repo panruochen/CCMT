1fc0cc1ed8a55128a5ad7daa91dd050bc1889e16
Pan Ruochen <ijkxyz@msn.com>
2015-03-11 16:59:40 +0800

    Update reclaim-mp3.py

--- reclaim-mp3.py
+++ reclaim-mp3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re, os, sys, glob, subprocess, codecs
+import getopt, shutil
 
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
@@ -7,54 +8,62 @@ from mutagen.flac import FLAC
 
 
 FF_PROBE   = 'd:/Tools/ffmpeg/bin/ffprobe'
-CACHE2_DIR = 'd:/PersonalConfigs/firefox-profiles/cache2/entries'
-#CACHE2_DIR = 'c:/Tools/Mozilla Firefox/configs/PRC/cache2/entries'
 
 PROBER = 'mutagen'
 #PROBER == 'ffprobe'
 
 #################################################################################
+class xencoding :
+    def __init__(self) :
+        self.rawbytes = None
+        self.encoding = None
+        self.string   = None
+    def set(self, b, e = 'utf-8') :
+        self.rawbytes = b
+        self.string =self.rawbytes.decode(e)
+        self.encoding = e
+    def iconv(self, e) :
+        try:
+            self.string = codecs.decode(self.rawbytes, 'utf-8').encode(e).decode('utf-8')
+            self.encoding = e
+            return True
+        except :
+            self.string = '<??>'
+            self.encoding = None
+#        print(codecs.decode(b'\xc3\xafL\xc3\x96\xc3\x90\xc3\x8a\xc3\x98\xc2\xba\xc3\xb2\xc3\x8a\xc3\x95\xc2\xb2\xc3\x98','utf-8').encode('utf_7').decode('utf-8'))
+        return False
+
 class Media :
     def __init__(self) :
         self.name     = None
         self.type     = None
-        self.artist   = None
-        self.title    = None
+        self.artist   = xencoding()
+        self.title    = xencoding()
         self.bitrate  = None
         self.duration = None
     def valid(self) :
         return (self.name is not None) and (self.type is not None) and (self.bitrate is not None) and (self.duration is not None)
-    def iconv(self, s) :
-        if type(s).__name__ == 'list' :
-            s1 = ''
-            for i in s :
-                s1 += i
-        elif type(s).__name__ == 'str' :
-            s1 = s
-        else :
-            raise Exception("Invalid type")
-        s1 = codecs.encode(s1, 'ascii', 'ignore')
-        for i in ['big5', 'gbk', 'gb2312', 'gb18030'] :
-            s2 = None
-            try :
-                s2 = s1.decode(i)
-            except:
-                pass
-            if s2 is not None :
-                print("String decoded as %s" % i, file=sys.stderr)
-                break
-        return s2
+
+    def guess_encoding(self, xe) :
+        languages = [ 'utf_7', 'big5', 'gbk' ]
+        for l in languages :
+            if xe.iconv(l) :
+               return True
+        return False
 
     def dump(self):
         print("%s: %s" % (self.name, self.type))
-        try :
-            print("  Artist: %s" % (self.artist))
-        except UnicodeEncodeError:
-            print("  Artist: %s" % self.iconv(self.artist))
         try:
-            print("  Title:  %s" % (self.title))
+            print("  Artist: %s" % (self.artist.string))
         except UnicodeEncodeError:
-            print("  Title: %s" % self.iconv(self.title))
+            self.guess_encoding(self.artist)
+            print("  Artist: %s" % (self.artist.string))
+        try:
+            print("  Title:  %s" % (self.title.string))
+        except UnicodeEncodeError:
+            self.guess_encoding(self.title)
+            print("  Title: %s" % (self.title.string))
+
         print("  Duration:  %s" % (self.duration))
         print("  Bitrate:  %s\n" % (self.bitrate))
 
@@ -116,34 +125,64 @@ class MediaProbeFFMPEG(MediaProbe) :
 class MediaProbeMutagen(MediaProbe) :
     def __init__(self) :
         MediaProbe.__init__(self)
+
+    def get_id3_info(self, id3, key) :
+        if key in id3 :
+            return id3[key]
+        else :
+            return None
+
     def get_info(self, file) :
-        audio = None
+        id3 = None
         type  = None
+        media = None
         try :
-            audio = MP3(file, ID3=EasyID3)
-            type = 'mp3'
+            id3 = MP3(file)
+            media = Media()
+            media.type     = 'mp3'
+            media.name     = file
+            media.duration = id3.info.length
+            media.bitrate  = id3.info.bitrate
+            media.title.set(bytes(id3['TIT2']))
+            media.artist.set(bytes(id3['TPE1']))
+#            print(media.title.rawbytes)
+#            print(media.artist.rawbytes)
         except :
             pass
 
-        if audio is None :
+        if media is None :
             try :
                 audio = FLAC(file)
                 type = 'flac'
             except :
                 pass
 
-        media = None
-        if audio is not None :
-            media = Media()
-            media.duration = audio.info.length
-            media.bitrate  = audio.info.bitrate
-            if 'title' in audio :
-                media.title    = audio['title']
-            if 'artist' in audio :
-                media.artist   = audio['artist']
-            media.name     = file
-            media.type     = type
         return media
+
+#===========================================================================
+# Main
+#===========================================================================
+
+sv_min_duration = 60
+sv_min_bitrate  = 192000
+sv_target_dir   = None
+sv_source_dir   = None
+
+try:
+    opts, args = getopt.gnu_getopt(sys.argv[1:], "d:t:f:", (['duration=', 'from-dir=', 'to-dir=']))
+except getopt.GetoptError as err:
+    print(str(err))
+    sys.exit(2)
+
+for o, a in opts:
+    if o == "-d" or o == '--duration':
+        sv_min_duration = int(a)
+    elif o == '-f' or o == '--from-dir':
+        sv_source_dir = a
+    elif o == '-t' or o == '--to-dir':
+        sv_target_dir = a
+    else:
+        raise Exception("Unknown option " + o)
 
 state = -1
 count = 0
@@ -155,32 +194,23 @@ if PROBER == 'mutagen' :
 elif PROBER == 'ffprobe' :
     mp = MediaProbeFfprobe()
 
+if sv_source_dir is None or sv_target_dir is None :
+    exit(0)
 
-"""
-filelist = []
-#for root,dirs,files in os.walk('d:/DevTools/global5.82') :
-for root,dirs,files in os.walk('d:/Media/==Music==') :
-    print(root)
-    print(dirs)
-    print(files)
-    filelist += [root + f for f in files]
-    filelist += []
-print("Totally %u files" % len(filelist))
-exit(0)
-"""
-
-CACHE2_DIR = 'd:/Media/==Music=='
-#CACHE2_DIR = 'd:/tmp'
-for root,dirs,files in os.walk(CACHE2_DIR) :
+for root,dirs,files in os.walk(sv_source_dir) :
     for f in files :
         count += 1
         file = root + '/' + f
-        print("Processing %06u %s" % (count,file))
+#        print("Processing %06u %s" % (count,file))
         media = mp.get_info(file)
         if media is not None :
             medialist.append(media)
 
 print("%u media files reclaimed from %u cached files\n" % (len(medialist),count))
-
+tr_table = { ord('"') : ' ', ord("'"):' ', ord('/'):' ', ord('\\'):' ', ord(':'):' ' };
 for media in medialist :
-    media.dump()
+    if media.duration >= sv_min_duration and media.bitrate >= sv_min_bitrate : #and media.title is not None :
+        print("Move %s to %s" % (media.name, sv_target_dir))
+        media.dump()
+#        pass
+        shutil.move(media.name, sv_target_dir + '/' + media.title.string.translate(tr_table) + '.' + media.type)
