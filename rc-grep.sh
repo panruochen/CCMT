a6faaf037929025b5d8322e0c261758422463b29
Pan Ruochen <ijkxyz@msn.com>
2015-04-02 11:38:28 +0800

    rc-grep: use getopts instead of getopt

--- rc-grep.sh
+++ rc-grep.sh
@@ -6,36 +6,48 @@
 #========================================================
 #set -e -o pipefail
 
-eval ARGV=($(getopt -l '' -o 'e:li' -- "$@")) || exit 1
-grep_options=()
-find_options=()
-for((i=0;i<${#ARGV[@]};i++)) {
-    o="${ARGV[$i]}"
-    case $o in
-    -e)
-        i=$((i+1));
-		a="${ARGV[$i]}"
-		grep_options+=("-e" "$a")
-		;;
-	-i)
-		grep_options+=("-i")
-		;;
-	-l)
-		grep_options+=("-l")
-		;;
-	--)
-        i=$((i+1));
-		break;;
-    esac
+usage_and_exit(xc)
+{
+	echo "Usage: $(basename $0) [-e PATTERN1] [-e PATTERN2] [-l] [-i] FILES1 FILES2..." >&2
+	exit(xc)
 }
 
-for((;i<${#ARGV[@]};i++)) {
+has_patterns=
+grep_options=()
+find_options=()
+while getopts ":e:li" o ;
+do
+    case "${o}" in
+    e)
+			grep_options+=("-e" "${OPTARG}")
+			has_patterns=y
+        ;;
+    l)
+			grep_options+=("-l")
+        ;;
+    i)
+			grep_options+=("-i")
+        ;;
+    *)
+		usage_and_exit(0)
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+##echo "$@" ; exit
+
+
+for a in "$@"
+do
 	if [ -n "$find_options" ]; then
 		find_options+=("-o")
 	fi
-	a="${ARGV[$i]}"
 	find_options+=("-name" "$a")
-}
+done
+
+if [ ${#find_options[@]} -eq 0 -o -z "$has_patterns" ]; then
+	usage_and_exit(0)
+fi
 
 set +e
 find \( -type f -a \( "${find_options[@]}" \) \) -print0| xargs -0 grep "${grep_options[@]}"
