c3707736f5d0e29ec73b9162465af88d7a37ad18
Pan Ruochen <ijkxyz@msn.com>
2016-12-15 19:03:56 +0800

    update guifont

--- remote-dir-sync.py
+++ remote-dir-sync.py
@@ -18,11 +18,11 @@ def get_md5sums(args, shell, fake_dir) :
     return md5sum
 
 def run_md5sum_locally() :
-    cmdline = 'cd %s && md5sum -b *' % local_ufs
-    return get_md5sums(cmdline, True, local_ufs)
+    cmdline = 'cd %s && md5sum -b *' % gv_local_ufs
+    return get_md5sums(cmdline, True, gv_local_ufs)
 
-def run_md5sum_remotely(host, remote_dir, amss1) :
-    args = ['ssh', host, 'cd %s && md5sum -b *' % remote_dir]
+def run_md5sum_remotely(remote_host, remote_dir, amss1) :
+    args = ['ssh', remote_host, 'cd %s && md5sum -b *' % remote_dir]
     return get_md5sums(args, False, amss1)
 
 def dict_get_diff(a, b) :
@@ -32,8 +32,11 @@ def dict_get_diff(a, b) :
             c[x] = a[x]
     return c
 
-local_ufs = os.environ['MY_UFS']
-if not local_ufs :
+#===============================================================================
+#  The main function starts here
+#===============================================================================
+gv_local_ufs = os.environ['MY_UFS']
+if not gv_local_ufs :
     print >> sys.stderr, "MY_UFS not set"
     exit(2)
 
@@ -41,18 +44,24 @@ amss1 = os.environ['AMSS1']
 if not amss1 :
     print >> sys.stderr, "AMSS1 not set"
     exit(2)
-remote_ufs = amss1 + '/release/ufs'
-a = remote_ufs.split(':')
-host = a[0]
+gv_remote_ufs = amss1 + '/release/ufs'
+a = gv_remote_ufs.split(':')
+remote_host = a[0]
 remote_dir = a[1]
 
 local_md5sums  = run_md5sum_locally()
-remote_md5sums = run_md5sum_remotely(host, remote_dir, amss1)
-c = dict_get_diff(remote_md5sums, local_md5sums)
+remote_md5sums = run_md5sum_remotely(remote_host, remote_dir, amss1)
+diffs = dict_get_diff(remote_md5sums, local_md5sums)
 
-print "Syncing %u files from remote server" % len(c)
-if len(c) > 0 :
-    for x in c :
-        args = ['scp', remote_ufs + '/' + x, local_ufs + '/' + x]
-        print args
+print "Syncing %u files from remote server" % len(diffs)
+if len(diffs) > 0 :
+    for x in diffs :
+        args = ['scp', gv_remote_ufs + '/' + x, gv_local_ufs + '/' + x]
         sp = subprocess.check_call(args)
+
+        local_md5sums  = run_md5sum_locally()
+        remote_md5sums = run_md5sum_remotely(remote_host, remote_dir, amss1)
+        diffs = dict_get_diff(remote_md5sums, local_md5sums)
+    if len(diffs) > 0 :
+        print >>sys.stderr, "Syncing failed !! %u files remain unsync"
+        exit(2)
